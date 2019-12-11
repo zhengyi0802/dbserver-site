d8cb3f6c49064354db06ad64ab8b4da09e72cb8c
Chevy Lin <chevylin0802@gmail.com>
2019-12-11 10:08:16 +0000

    update codes

--- dbserver/mobile/querydata.php
+++ dbserver/mobile/querydata.php
@@ -7,6 +7,13 @@
        $sql  = "SELECT city.id AS id, city.name AS city, state.name AS state, country.name AS country FROM city ";
        $sql .= "LEFT JOIN country ON city.country_id = country.id ";
        $sql .= "LEFT JOIN state ON city.state_id = state.id WHERE 1";
+    } else if ($tablename == 'state') {
+       $sql  = "SELECT state.id AS id, country.name AS country, state.name AS state FROM state ";
+       $sql .= "LEFT JOIN country ON state.country_id = country.id WHERE 1";
+    } else if ($tablename == 'district') {
+       $sql  = "SELECT district.id AS id, country.name AS country, city.name AS city, district.name AS district FROM district ";
+       $sql .= "LEFT JOIN country ON district.country_id = country.id ";
+       $sql .= "LEFT JOIN city ON district.city_id = city.id WHERE 1";
     } else {
        $sql = "SELECT * FROM `".$tablename."` WHERE 1";
     }
